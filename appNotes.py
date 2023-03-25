1dd24f8bd345a0b686dd082a1bde47bfb4c99b1b
bsacus <114698872+bsacus@users.noreply.github.com>
2023-03-25 14:04:47 -0400

    Create appNotes.py
    Test

---
+++ appNotes.py
@@ -0,0 +1,24 @@
+import subprocess
+
+def create_note_in_notes_app(note_title, note_body):
+    """
+    Create a new note in the Notes app on a Mac.
+
+    Parameters:
+    - note_title (str): The title of the note.
+    - note_body (str): The body of the note.
+
+    Returns:
+    - None.
+    """
+    # Use AppleScript to open the Notes app
+    subprocess.call(['osascript', '-e', 'tell application "Notes" to activate'])
+
+    # Use AppleScript to create a new note with the specified title and body
+    script = f'tell application "Notes" to make new note with properties {{name:"{note_title}", body:"{note_body}"}}'
+    subprocess.call(['osascript', '-e', script])
+
+# Example usage
+note_title = "Test Note1"
+note_body = "This is a test note created with Python."
+create_note_in_notes_app(note_title, note_body)
